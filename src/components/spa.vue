fec682b3e07a3abca603fac1bb3f36a7d942fbc5
priscilafernandes <pris1@outlook.com.br>
2018-09-29 20:53:14 -0300

    firebase funcionando e estilos aplicados

--- src/components/spa.vue
+++ src/components/spa.vue
@@ -1,44 +1,64 @@
 <template>
   <main class="container-fluid">
-    <h1 class="hello">Shopping</h1>
+    <header class="navbar navbar-expand-lg bg-dark">
+      <h1 class="text-light">Bookstore</h1>
+      <nav class="col-sm">
+        <div class="row mt-3">
+          <form class="col-sm">
+            <div class="form-group">
+              <input v-model="search" type="text" class="form-control" placeholder="Digite aqui...">
+            </div>
+          </form>        
+          <div class="col-sm-3">
+            <div class="form-group">
+              <button @click.prevent.stop="getData()" type="submit" class="btn btn-outline-light">Buscar</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
 
-    <div class="row">
-      <form class="col">
-        <div class="form-group">
-          <input v-model="search" type="text" class="form-control" placeholder="Busque aqui...">
-        </div>
-      </form>
-      
-      <div class="col-3">
-        <div class="form-group">
-          <button @click.prevent.stop="getData()" type="submit" class="btn btn-primary">Buscar</button>
-        </div>
+    <section class="table-responsive" v-if="!selectedBook.id">
+      <div class="navbar navbar-expand-lg">
+        <p>Você buscou por "<b>{{ search }}</b>"</p>
       </div>
-    </div>
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col-sm">Capa</th>
+            <th scope="col-sm">Título</th>
+            <th scope="col-sm">Preço</th>
+            <th scope="col-sm">Info</th>
+            <th scope="col-sm">Comprar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- se quantidade for positiva -->
+          <tr v-if="!!books.length" v-for="book in books" :key="book.thumbnail">
+            <th scope="row"><img :src="book.thumbnail"></th>
+            <td>{{ book.title }}</td>
+            <td>R$ {{ book.price }}</td>
+            <td><a @click.prevent.stop="getDataItem(book.id)" :href="book.id" class="badge badge-info">Mais Detalhes {{ book.id }}</a></td>
+            <td><a :href="book.permalink" target="_blank">Comprar</a></td>
+          </tr>
+          <!-- se quantidade for negativa -->
+          <tr v-if="!!!books.length">
+            <td colspan="5">Sem resultados...</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
 
-    <div>
-      <p>Você buscou por "{{ search }}"</p>
-    </div>
-
-    <template>
-      <ul class="list-group list-group-flush" v-if="!!results.length" v-for="result in results" :key="result.thumbnail">
-        <li class="list-group-item">{{ result.title }}</li>
-        <li><img :src="result.thumbnail"></li>
-        <!-- <li class="list-group-item"><a @click.prevent.stop="getDataItem(result.id)" href="">{{ result.id }}</a></li> -->
-      </ul>
-    </template>
-
-    <!-- <template>
-      <ul class="list-group list-group-flush" v-if="!!results.length" v-for="result in results" :key="result.id">
-        <li class="list-group-item"><a href="">{{ result.id }}</a>
-        <li class="list-group-item">{{ result.name }}</li>
-      </ul>
-    </template> -->
-
-    <ul class="list-group list-group-flush" v-if="!!!results.length">
-      <li class="list-group-item"><p>Resultado não encontrado</p></li>
-    </ul>
-
+    <!-- detalhe do produto -->
+    <section class="results-field" v-if="selectedBook.id">
+      <div>
+        <div><img :src="selectedBook.pictures.secure_url"></div>
+        <h2>{{ selectedBook.title }}</h2>        
+        <p>R$ {{ selectedBook.price }}</p>
+        <p><a :href="selectedBook.permalink" target="_blank">Comprar</a></p>
+        <button @click.prevent.stop="backBooks()"></button>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -48,42 +68,42 @@
   export default {
     name: 'SPA',
     
+    // variáveis
     data() {
       return {
         search: '',
-        results: [],
-        selectedItem: {},
+        books: [],
+        selectedBook: {},
       }
     },
 
+    // funções
     methods: {
       getData() {
-        if(this.search) {
+        if(this.search) {          
+          const url = `https://api.mercadolibre.com/sites/MLB/search?q=${this.search}&category=MLB1196`;
           const vm = this;
 
-          const url = `https://api.mercadolibre.com/sites/MLB/search?q={{search}}&category=MLB1196`;
-  
           axios.get(url).then((response) => {
-            vm.results = response.data.results;
-            // vm.results = response.data;
+            vm.books = response.data.results;
           });
         }
       },
 
-
-      /*
       getDataItem(item) {
         if(this.search) {
+          const url = `https://api.mercadolibre.com/items/${item}`;
           const vm = this;
-          // const url = `https://jsonplaceholder.typicode.com/posts/${item}`;
-          // const url = `https://api.mercadolibre.com/sites/MLB/search?category=${item}`;
           
           axios.get(url).then((response) => {
-            vm.selectedItem = response.data;
+            vm.selectedBook = response.data;
           });
         }
       },
-      */
+
+      backBooks() {
+        this.selectedBook = {};
+      }
 
     }
   }
